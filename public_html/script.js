4176957b71e4a8ff720930e2235bd90349a6c5fd
Esme Grundy <eg2485@gmail.com>
2015-01-07 11:29:25 -0800

    Milestones 2 and 3

--- public_html/script.js
+++ public_html/script.js
@@ -4,6 +4,26 @@
  * and open the template in the editor.
  */
 
-$("document").ready(function(){
+$("document").ready(function() {
     $(".class1").css("color", "orange");
+    $("div p:last-child").css("background-color", "yellow");
+    $("#chelsea").css({"width": "400px", "height": "auto"});
+
+    $("#alertButton").bind("click", alertButtonClick);
+    $("#chelsea").bind("mouseover", mouseOverMe)
+            .bind("mouseout", mouseOutMe);
 });
+
+function alertButtonClick() {
+    alert("There was a button clicked");
+}
+
+function mouseOverMe()
+{
+    $("#first").html("You put your cursor on my logo");
+}
+
+function mouseOutMe()
+{
+    $("#first").html("You left my logo");
+}
