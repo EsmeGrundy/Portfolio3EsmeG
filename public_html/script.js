f7e566715dfc038c6b748f54fc308fa657bad28f
Esme Grundy <eg2485@gmail.com>
2015-01-08 11:27:25 -0800

    Milestone 4 and start of Milestone 5

--- public_html/script.js
+++ public_html/script.js
@@ -12,6 +12,15 @@ $("document").ready(function() {
     $("#alertButton").bind("click", alertButtonClick);
     $("#chelsea").bind("mouseover", mouseOverMe)
             .bind("mouseout", mouseOutMe);
+
+    $("#replaceHTML").bind("click", replaceHTML);
+    $("#addPara").bind("click", addPara);
+    $("#removePara").bind("click", removePara);
+
+    $("#hideLogo").bind("click", hideLogo);
+    $("#showLogo").bind("click", showLogo);
+    $("#highLogo").bind("click", highLogo);
+
 });
 
 function alertButtonClick() {
@@ -27,3 +36,28 @@ function mouseOutMe()
 {
     $("#first").html("You left my logo");
 }
+
+function replaceHTML() {
+    $("#h3Tag").html("<h6>New H6 Tag!!!!!</h6>");
+}
+
+function addPara() {
+    $("#randPara").append("<p>Another Paragraph</p>");
+}
+
+function removePara() {
+    $("#randPara p:last-child").remove();
+}
+
+function hideLogo() {
+    $("#chelsea").hide("puff", {}, 2500);
+}
+
+function showLogo() {
+    $("#chelsea").show("fold", {}, 2500);
+}
+
+function highLogo() {
+    $("#logo").effect("highlight", {color: "purple"}, 500);
+}
+
