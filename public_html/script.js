a01e464ec99e026004a4172ad0a65e3f377f4b11
Esme Grundy <eg2485@gmail.com>
2015-01-12 12:15:02 -0800

    Milestone 6

--- public_html/script.js
+++ public_html/script.js
@@ -8,6 +8,10 @@ $("document").ready(function() {
     $(".class1").css("color", "orange");
     $("div p:last-child").css("background-color", "yellow");
     $("#chelsea").css({"width": "400px", "height": "auto"});
+    $("#slide").css({"position": "relative", "height": "200px", "width": "200px"});
+    $("#slide div").css({"position": "absolute", "z-index": "0"});
+    $("#slide div.prev").css({"z-index": "1"});
+    4("#slide div.active").css({"z-index": "2"});
 
     $("#alertButton").bind("click", alertButtonClick);
     $("#chelsea").bind("mouseover", mouseOverMe)
@@ -22,10 +26,8 @@ $("document").ready(function() {
     $("#showLogo").bind("click", showLogo);
     $("#highLogo").bind("click", highLogo);
     
-    $("#essay > p").hide();
-    $("#essay h4").click(function (){
-        
-    });
+   $("#essay").accordion({header: "h4"});
+   setInterval("slide()", 2000);
 
 });
 
@@ -69,5 +71,17 @@ function showLogo() {
 
 function highLogo() {
     $("#logo").effect("highlight", {color: "purple"}, 500);
+}
+
+function slide(){
+    var current = $("#slide div.active");
+    var next = current.next();
+    if(next.length === 0){
+        next = $("#slide div:first");
+    }
+    current.removeClass("active").addClass("prev");
+    next.css({opacity: 0.0}).addClass("active").animate({opacity: 1.0}, 2000, function(){
+        current.removeClass("prev");
+    });
 }
 
